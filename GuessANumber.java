14fb4dda04b755722f06709be86e8ac643c4069d
Yordan <y.popov1516@gmail.com>
2024-01-21 21:27:14 +0200

    Adding second level and option to start new game

--- GuessANumber.java
+++ GuessANumber.java
@@ -11,7 +11,11 @@ public class GuessANumber {
 
         int computerNumber = random.nextInt(99) +1;
 
-        while (true){
+        boolean levelOne = true;
+        boolean levelTwo = false;
+
+
+        while (levelOne){
 
             System.out.print("Guess the number between 1 and 100:");
             String playerInput = scanner.nextLine();
@@ -31,7 +35,16 @@ public class GuessANumber {
                 playerNumber = Integer.parseInt(playerInput);
                 if (playerNumber == computerNumber){
                     System.out.println("Bravo! You guessed it");
-                    break;
+                    System.out.println("Next level? y / n");
+                    playerInput = scanner.nextLine();
+
+                    if (playerInput.equals("y") || playerInput.equals("yes")){
+
+                        levelTwo = true;
+                        levelOne = false;
+                    } else if (playerInput.equals("n") || playerInput.equals("no")) {
+                        levelOne = false;
+                    }
                 }else if( playerNumber < computerNumber){
                     System.out.println("Too Low\n");
                 }else if (playerNumber > computerNumber){
@@ -39,6 +52,49 @@ public class GuessANumber {
                 }
 
                 }else {
+                System.out.println("Invalid input!");
+            }
+        }
+
+        computerNumber = random.nextInt(199) + 1;
+
+
+        while (levelTwo){
+
+            System.out.print("Guess the number between 1 and 200:");
+            String playerInput = scanner.nextLine();
+
+            int playerNumber;
+
+            boolean isValid = true;
+
+            for (int i = 1; i < playerInput.length(); i++) {
+                if (playerInput.charAt(i) < 48 || playerInput.charAt(i) >57 ){
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if (isValid){
+                playerNumber = Integer.parseInt(playerInput);
+                if (playerNumber == computerNumber){
+                    System.out.println("Bravo! You guessed it");
+                    System.out.println("New game? y / n");
+                    playerInput = scanner.nextLine();
+
+                    if (playerInput.equals("y") || playerInput.equals("yes")){
+                        levelOne = true;
+                        levelTwo = false;
+                    } else if (playerInput.equals("n") || playerInput.equals("no")) {
+                        levelTwo = false;
+                    }
+                }else if( playerNumber < computerNumber){
+                    System.out.println("Too Low\n");
+                }else if (playerNumber > computerNumber){
+                    System.out.println("Too High\n");
+                }
+
+            }else {
                 System.out.println("Invalid input!");
             }
         }
